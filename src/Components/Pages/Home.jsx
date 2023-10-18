6737cbfce2777a3fac2bd4498b3b16254fa6aa01
Ariful2634 <80531678+Ariful2634@users.noreply.github.com>
2023-10-18 18:16:07 +0600

    adding an another section on the home page

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -1,5 +1,6 @@
 import Brands from "../../Brands/Brands";
 import Banner from "./Banner";
+import HotDeals from "./HotDeals";
 import Sale from "./Sale";
 
 
@@ -10,6 +11,7 @@ const Home = () => {
             <Banner></Banner>
             <Brands></Brands>
             <Sale></Sale>
+            <HotDeals></HotDeals>
         </div>
     );
 };
---
+++ src/Components/Pages/HotDeals.jsx
@@ -0,0 +1,52 @@
+import Marquee from "react-fast-marquee";
+import mac from '../Pages../../../assets/mac.jpg'
+import pixel from '../Pages../../../assets/pixel.jpg'
+import oppo from '../Pages../../../assets/oppo.jpg'
+
+
+const HotDeals = () => {
+    return (
+        <div className="mt-16">
+            <h2 className="font-bold text-center text-4xl text-blue-600 mb-8">Hot Deals</h2>
+            <Marquee pauseOnHover={true}>
+            <div className="mr-20">
+                <img className="w-[300px] h-[200px]" src={mac} alt="" />
+                <h2 className='font-bold text-center text-2xl mb-3 mt-3'>Macbook Air</h2>
+                <div className='flex gap-4 justify-center mb-3'>
+                    <p className='font-bold'>$̶1̶8̶0̶0̶0̶0̶ </p>
+                    <p className='font-bold '>$80000</p>
+                </div>
+                <div className='text-center'>
+                    <button className='btn'>Shop Now</button>
+                </div>
+            </div>
+            <div className="mr-20">
+                <img className="w-[300px]  h-[200px]" src={pixel} alt="" />
+                <h2 className='font-bold text-center text-2xl mb-3 mt-3'>Pixel-7</h2>
+                <div className='flex gap-4 justify-center mb-3'>
+                    <p className='font-bold'>$̶4̶5̶0̶0̶0̶ </p>
+                    <p className='font-bold '>$25000</p>
+                </div>
+                <div className='text-center'>
+                    <button className='btn'>Shop Now</button>
+                </div>
+            </div>
+            <div className="mr-20">
+                <img className="w-[300px]  h-[200px]" src={oppo} alt="" />
+                <h2 className='font-bold text-center text-2xl mb-3 mt-3'>Oppo Air Buds</h2>
+                <div className='flex gap-4 justify-center mb-3'>
+                    <p className='font-bold'>$̶3̶0̶0̶0̶ </p>
+                    <p className='font-bold '>$1000</p>
+                </div>
+                <div className='text-center'>
+                    <button className='btn'>Shop Now</button>
+                </div>
+            </div>
+            </Marquee>
+            
+            
+        </div>
+    );
+};
+
+export default HotDeals;
